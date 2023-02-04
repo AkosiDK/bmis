937737224426e06840da7dd943756865bfce67a6
AkosiDK <darrylkayeabellasanga@gmail.com>
2023-02-04 14:59:08 +0800

    added blotter add modal

--- resources/views/brgyblotter.blade.php
+++ resources/views/brgyblotter.blade.php
@@ -165,6 +165,99 @@
                                 <x-fas-plus class="w-4 h-4 mr-1" fill="white" />
                                 Blotter
                             </button>
+
+                            <!-- Add Blotter Modal -->
+                            <div id="addblotter-modal" tabindex="-1" class="fixed top-0 left-0 right-0 z-50 hidden w-full p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-modal md:h-full">
+                                <div class="relative w-full h-full max-w-4xl md:h-auto">
+                                    <!-- Modal content -->
+                                    <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
+                                        <!-- Modal header -->
+                                        <div class="flex items-center justify-between p-5 border-b rounded-t dark:border-gray-600 bg-gradient-to-r from-blue-600 to-blue-500">
+                                            <h3 class="text-xl font-medium text-white dark:text-white">
+                                                Add - Blotter
+                                            </h3>
+                                            <button type="button" class="text-white bg-transparent hover:bg-blue-600 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-hide="addblotter-modal">
+                                                <svg aria-hidden="true" class="w-5 h-5" fill="white" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                                                    <path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path>
+                                                </svg>
+                                                <span class="sr-only">Close modal</span>
+                                            </button>
+
+                                        </div>
+                                        <!-- Modal body -->
+                                        <form class="space-y-6" action="#">
+                                            <div class="grid grid-cols-2">
+                                                <div class="col-span-1">
+                                                    <div class="py-6 px-6 space-y-3">
+                                                        <!-- Complainant Name -->
+                                                        <label for="addblottercomplainantname" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Complainant Name</label>
+                                                        <input type="text" name="addblottercomplainantname" id="addblottercomplainantname" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" placeholder="Enter Complainant Name" required>
+
+                                                        <!-- Victims Name -->
+                                                        <label for="addblottervictimsname" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Victim(s) Name </label>
+                                                        <input type="text" name="addblottervictimsname" id="addblottervictimsname" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" placeholder="Enter Victim(s) Name" required>
+
+                                                        <!-- Incident -->
+                                                        <label for="addblotterincident" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Type of Incident </label>
+                                                        <input type="text" name="addblotterincident" id="addblotterincident" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" placeholder="Enter Incident Type" required>
+
+                                                        <!-- Incident Location -->
+                                                        <label for="addblotterincidentlocation" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Incident Location </label>
+                                                        <input type="text" name="addblotterincidentlocation" id="addblotterincidentlocation" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" placeholder="Enter Incident Location" required>
+
+                                                    </div>
+                                                </div>
+                                                <div class="col-span-1">
+                                                    <div class="py-6 pr-6 space-y-3">
+                                                        <!-- Respondent Name -->
+                                                        <label for="addblotterrespondentname" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Respondent Name</label>
+                                                        <input type="text" name="addblotterrespondentname" id="addblotterrespondentname" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" placeholder="Enter Respondent Name" required>
+
+                                                        <!-- Date of Incident -->
+                                                        <label for="addblotterdateofincident" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Date of Incident</label>
+                                                        <div class="relative">
+                                                            <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+                                                                <svg aria-hidden="true" class="w-5 h-5 text-gray-500 dark:text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                                                                    <path fill-rule="evenodd" d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z" clip-rule="evenodd"></path>
+                                                                </svg>
+                                                            </div>
+                                                            <input datepicker type="text" id="addblotterdateofincident" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Select Date of Incident">
+                                                        </div>
+
+                                                        <!-- Time of Incident -->
+                                                        <label for="addblottertimeofincident" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Time of Incident</label>
+                                                        <input type="time" id="addblottertimeofincident" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Select Time of Incident">
+
+                                                        <!-- Blotter Status -->
+                                                        <label for="addblotterstatus" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Status</label>
+                                                        <select id="addblotterstatus" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                                                            <option selected>Select Blotter Status</option>
+                                                            <option>Active</option>
+                                                            <option>Settled</option>
+                                                            <option>Scheduled</option>
+                                                            <option>Unscheduled</option>
+                                                        </select>
+
+                                                    </div>
+                                                </div>
+                                                <div class="col-span-2">
+                                                    <div class="px-6 pb-6">
+                                                        <!-- Details -->
+                                                        <label for="addbrgybusspermremarks" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Details</label>
+                                                        <textarea id="addbrgybusspermremarks" rows="2" class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Enter more details here..." required></textarea>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </form>
+                                        <!-- Modal footer -->
+                                        <div class="flex items-center p-6 space-x-2 border-t border-gray-200 rounded-b dark:border-gray-600 justify-end">
+                                            <button data-modal-hide="addblotter-modal" type="button" class="text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-gray-200 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-600">Cancel</button>
+                                            <button data-modal-hide="addblotter-modal" type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Save</button>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+
                         </div>
                     </div>
                 </div>

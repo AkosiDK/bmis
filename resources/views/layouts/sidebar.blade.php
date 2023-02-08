5d1545ecb94c3b96150ddcda9af807e2334a443f
AkosiDK <darrylkayeabellasanga@gmail.com>
2023-02-08 11:23:09 +0800

    updated icon sidenav

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -24,7 +24,7 @@
             <!-- Profile Info -->
             <div>
                 <div>
-                    <h5 class="overflow-hidden text-xs font-normal whitespace-nowrap overflow-ellipsis text-white">
+                    <h5 class="overflow-hidden text-xs font-normal whitespace-nowrap overflow-ellipsis text-white font-bold">
                         ADMINISTRATOR
                     </h5>
                 </div>
@@ -35,18 +35,14 @@
     <div class="flex flex-col mb-0 ml-0">
         <x-sidebar-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
             <x-slot name="icon">
-                <svg xmlns="http://www.w3.org/2000/svg" class="w-4 h-4 mr-4" fill="none" viewBox="0 0 24 24" stroke="#fff" stroke-width="2">
-                    <path stroke-linecap="round" stroke-linejoin="round" d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6" />
-                </svg>
+                <x-fas-home class="w-4 mr-4 fill-white"/>
             </x-slot>
             <p class="text-white">{{ __('Dashboard') }}</p>
         </x-sidebar-nav-link>
 
         <x-sidebar-nav-link :href="route('officials')" :active="request()->routeIs('officials')">
             <x-slot name="icon">
-                <svg xmlns="http://www.w3.org/2000/svg" class="w-4 h-4 mr-4" fill="none" viewBox="0 0 24 24" stroke="#fff" stroke-width="2">
-                    <path stroke-linecap="round" stroke-linejoin="round" d="M21 13.255A23.931 23.931 0 0112 15c-3.183 0-6.22-.62-9-1.745M16 6V4a2 2 0 00-2-2h-4a2 2 0 00-2 2v2m4 6h.01M5 20h14a2 2 0 002-2V8a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z" />
-                </svg>
+                <x-fas-user-tie class="w-4 mr-4 fill-white"/>
             </x-slot>
             <p class="text-white">{{ __('Barangay Officials') }}</p>
         </x-sidebar-nav-link>
@@ -55,30 +51,21 @@
 
         <x-sidebar-nav-link :href="route('residents')" :active="request()->routeIs('residents')">
             <x-slot name="icon">
-                <svg xmlns="http://www.w3.org/2000/svg" class="w-4 h-4 mr-4" fill="none" viewBox="0 0 24 24" stroke="#fff" stroke-width="2">
-                    <path stroke-linecap="round" stroke-linejoin="round" d="M10.325 4.317c.426-1.756 2.924-1.756 3.35 0a1.724 1.724 0 002.573 1.066c1.543-.94 3.31.826 2.37 2.37a1.724 1.724 0 001.065 2.572c1.756.426 1.756 2.924 0 3.35a1.724 1.724 0 00-1.066 2.573c.94 1.543-.826 3.31-2.37 2.37a1.724 1.724 0 00-2.572 1.065c-.426 1.756-2.924 1.756-3.35 0a1.724 1.724 0 00-2.573-1.066c-1.543.94-3.31-.826-2.37-2.37a1.724 1.724 0 00-1.065-2.572c-1.756-.426-1.756-2.924 0-3.35a1.724 1.724 0 001.066-2.573c-.94-1.543.826-3.31 2.37-2.37.996.608 2.296.07 2.572-1.065z" />
-                    <path stroke-linecap="round" stroke-linejoin="round" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
-                </svg>
+            <x-fas-user class="w-4 mr-4 fill-white"/>
             </x-slot>
             <p class="text-white">{{ __('Barangay Residents') }}</p>
         </x-sidebar-nav-link>
 
         <x-sidebar-nav-link :href="route('residentrequest')" :active="request()->routeIs('residentrequest')">
             <x-slot name="icon">
-                <svg xmlns="http://www.w3.org/2000/svg" class="w-4 h-4 mr-4" fill="none" viewBox="0 0 24 24" stroke="#fff" stroke-width="2">
-                    <path stroke-linecap="round" stroke-linejoin="round" d="M10.325 4.317c.426-1.756 2.924-1.756 3.35 0a1.724 1.724 0 002.573 1.066c1.543-.94 3.31.826 2.37 2.37a1.724 1.724 0 001.065 2.572c1.756.426 1.756 2.924 0 3.35a1.724 1.724 0 00-1.066 2.573c.94 1.543-.826 3.31-2.37 2.37a1.724 1.724 0 00-2.572 1.065c-.426 1.756-2.924 1.756-3.35 0a1.724 1.724 0 00-2.573-1.066c-1.543.94-3.31-.826-2.37-2.37a1.724 1.724 0 00-1.065-2.572c-1.756-.426-1.756-2.924 0-3.35a1.724 1.724 0 001.066-2.573c-.94-1.543.826-3.31 2.37-2.37.996.608 2.296.07 2.572-1.065z" />
-                    <path stroke-linecap="round" stroke-linejoin="round" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
-                </svg>
+            <x-fas-file-lines class="w-4 mr-4 fill-white"/>
             </x-slot>
             <p class="text-white">{{ __('Barangay Residents Request') }}</p>
         </x-sidebar-nav-link>
 
         <x-sidebar-nav-link :href="route('settings')" :active="request()->routeIs('settings')">
             <x-slot name="icon">
-                <svg xmlns="http://www.w3.org/2000/svg" class="w-4 h-4 mr-4" fill="none" viewBox="0 0 24 24" stroke="#fff" stroke-width="2">
-                    <path stroke-linecap="round" stroke-linejoin="round" d="M10.325 4.317c.426-1.756 2.924-1.756 3.35 0a1.724 1.724 0 002.573 1.066c1.543-.94 3.31.826 2.37 2.37a1.724 1.724 0 001.065 2.572c1.756.426 1.756 2.924 0 3.35a1.724 1.724 0 00-1.066 2.573c.94 1.543-.826 3.31-2.37 2.37a1.724 1.724 0 00-2.572 1.065c-.426 1.756-2.924 1.756-3.35 0a1.724 1.724 0 00-2.573-1.066c-1.543.94-3.31-.826-2.37-2.37a1.724 1.724 0 00-1.065-2.572c-1.756-.426-1.756-2.924 0-3.35a1.724 1.724 0 001.066-2.573c-.94-1.543.826-3.31 2.37-2.37.996.608 2.296.07 2.572-1.065z" />
-                    <path stroke-linecap="round" stroke-linejoin="round" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
-                </svg>
+            <x-fas-gear class="w-4 mr-4 fill-white" />
             </x-slot>
             <p class="text-white">{{ __('Settings') }}</p>
         </x-sidebar-nav-link>

179086d2c129923baec9f268bcfc8227d44f87d5
AkosiDK <darrylkayeabellasanga@gmail.com>
2023-02-11 17:57:30 +0800

    updated userdocument form

--- resources/views/userdocument.blade.php
+++ resources/views/userdocument.blade.php
@@ -306,6 +306,12 @@
           <h5 class="text-xl font-medium leading-normal text-white" id="brgycertificatesLabel">
             Request Form
           </h5>
+          <button type="button" class="text-gray-400 bg-transparent hover:bg-blue-600 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-600 dark:hover:text-white" data-bs-dismiss="modal">
+            <svg aria-hidden="true" class="w-5 h-5" fill="white" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+              <path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path>
+            </svg>
+            <span class="sr-only">Close modal</span>
+          </button>
         </div>
         <div class="modal-body relative p-4">
 
@@ -353,7 +359,7 @@
       </div>
     </div>
   </div>
-  
+
   <!-- ====== Footer Section Start -->
   <footer class="relative z-10 bg-gradient-to-r from-red-700  to-blue-700 pt-20 pb-10 lg:pt-[120px] lg:pb-20">
     <div class="container mx-auto">

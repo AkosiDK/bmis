6e2f6be9a9f348b61ac7d9556fc3e9fdd9b0dd2c
AkosiDK <darrylkayeabellasanga@gmail.com>
2023-02-13 16:46:17 +0800

    updated index

--- resources/views/resident/index.blade.php
+++ resources/views/resident/index.blade.php
@@ -110,7 +110,7 @@
                                             </a>
                                         </div>
                                     </th>
-                        
+
                                     <th scope="col" class="px-6 py-3">
                                         <div class="flex items-center">
                                             Civil Status
@@ -182,7 +182,7 @@
                                     <td class="px-6 py-2 text-clip overflow-hidden ...">
                                         {{$resident->birthdate }}
                                     </td>
-                        
+
                                     <td class="px-6 py-2 text-clip overflow-hidden ...">
                                         {{$resident->civilstatus }}
                                     </td>
@@ -196,13 +196,60 @@
                                         Zone 6B, Baluarte, Tagoloan, Mis. Or.
                                     </td>
                                     <td class="px-6 py-2">
-                                        <span
-                                            class="bg-green-600 text-white text-xs font-medium items-center file:mr-2 px-2.5 py-0.5 rounded-full"
-                                            title="Official is Active">
+                                        <span class="bg-green-600 text-white text-xs font-medium items-center file:mr-2 px-2.5 py-0.5 rounded-full" title="Official is Active">
                                             {{$resident->alivedordeceased }}
                                         </span>
                                     </td>
                                     {{-- TODO: --}}
+                                    <td class="px-6 py-2">
+                                        <!-- Update Modal -->
+                                        <button data-modal-target="editresident-modal" data-modal-toggle="editresident-modal" class="inline-flex items-center hover:bg-blue-400 focus:ring-2 focus:outline-none  focus:ring-blue-500" title="Edit Resident">
+                                            <x-fas-square-pen class="w-4 h-4  fill-blue-600" />
+                                        </button>
+                                        <!-- Insert Update Modal -->
+
+                                        <livewire:residents.updateresident>
+
+                                            <!-- View Resident Modal -->
+                                            <button data-modal-target="viewresident-modal" data-modal-toggle="viewresident-modal" class="inline-flex items-center hover:bg-green-400 focus:ring-2 focus:outline-none  focus:ring-green-500" title="View Resident">
+                                                <x-fas-eye class="w-4 h-4  fill-green-600" />
+                                            </button>
+
+                                            <!-- Insert View Modal -->
+
+                                            <livewire:residents.viewresident>
+
+                                                <!-- Delete Default Modal -->
+                                                <button data-modal-target="popup-modal" data-modal-toggle="popup-modal" class="inline-flex items-center hover:bg-red-400 focus:ring-2 focus:outline-none  focus:ring-red-500" title="Delete Official">
+                                                    <x-fas-square-xmark class=" w-4 h-4 fill-red-600" />
+                                                </button>
+
+                                                <!-- Delete Default Modal -->
+                                                <div id="popup-modal" tabindex="-1" class="fixed top-0 left-0 right-0 z-50 hidden p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-modal md:h-full">
+                                                    <div class="relative w-full h-full max-w-md md:h-auto">
+                                                        <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
+                                                            <button type="button" class="absolute top-3 right-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-800 dark:hover:text-white" data-modal-hide="popup-modal">
+                                                                <svg aria-hidden="true" class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                                                                    <path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path>
+                                                                </svg>
+                                                                <span class="sr-only">Close modal</span>
+                                                            </button>
+                                                            <div class="p-6 text-center">
+                                                                <svg aria-hidden="true" class="mx-auto mb-4 text-red-500 w-14 h-14 dark:text-gray-200" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                                                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 8v4m0 4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path>
+                                                                </svg>
+                                                                <h3 class="mb-5 text-lg font-normal text-gray-500 dark:text-gray-400">Are you
+                                                                    sure you want to delete this Brgy. Official?</h3>
+                                                                <button data-modal-hide="popup-modal" type="button" class="text-white bg-red-600 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-sm inline-flex items-center px-5 py-2.5 text-center mr-2">
+                                                                    Yes, I'm sure
+                                                                </button>
+                                                                <button data-modal-hide="popup-modal" type="button" class="text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-gray-200 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-600">No,
+                                                                    cancel</button>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                    </td>
                                 </tr>
                                 @endforeach
                             </tbody>
@@ -211,25 +258,25 @@
 
                     </div>
                     <div class="flex items-center justify-between border-t border-gray-200 bg-white px-4 py-3 sm:px-6">
-                            <div class="flex flex-1 justify-between sm:hidden">
-                                <a href="#" class="relative inline-flex items-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 hover:bg-gray-50">Previous</a>
-                                <a href="#" class="relative ml-3 inline-flex items-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 hover:bg-gray-50">Next</a>
+                        <div class="flex flex-1 justify-between sm:hidden">
+                            <a href="#" class="relative inline-flex items-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 hover:bg-gray-50">Previous</a>
+                            <a href="#" class="relative ml-3 inline-flex items-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 hover:bg-gray-50">Next</a>
+                        </div>
+                        <div class="hidden sm:flex sm:flex-1 sm:items-center sm:justify-between">
+                            <div>
+                                <p class="text-sm text-gray-700">
+                                    Showing
+                                    <span class="font-medium">1</span>
+                                    to
+                                    <span class="font-medium">10</span>
+                                    of
+                                    <span class="font-medium">97</span>
+                                    results
+                                </p>
                             </div>
-                            <div class="hidden sm:flex sm:flex-1 sm:items-center sm:justify-between">
-                                <div>
-                                    <p class="text-sm text-gray-700">
-                                        Showing
-                                        <span class="font-medium">1</span>
-                                        to
-                                        <span class="font-medium">10</span>
-                                        of
-                                        <span class="font-medium">97</span>
-                                        results
-                                    </p>
-                                </div>
-                                <div>
-                                    {{ $residents->links() }}
-                                    {{-- <nav class="isolate inline-flex -space-x-px rounded-md shadow-sm" aria-label="Pagination">
+                            <div>
+                                {{ $residents->links() }}
+                                {{-- <nav class="isolate inline-flex -space-x-px rounded-md shadow-sm" aria-label="Pagination">
                                         <a href="#" class="relative inline-flex items-center rounded-l-md border border-gray-300 bg-white px-2 py-2 text-sm font-medium text-gray-500 hover:bg-gray-50 focus:z-20">
                                             <span class="sr-only">Previous</span>
                                             <!-- Heroicon name: mini/chevron-left -->
@@ -253,9 +300,9 @@
                                             </svg>
                                         </a>
                                     </nav> --}}
-                                </div>
                             </div>
                         </div>
+                    </div>
                 </div>
             </div>
         </div>
